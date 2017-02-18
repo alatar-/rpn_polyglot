d061c4791e049817ad5ebc91a2e1bdc14ab8bff5
alatar- <contact@alatar.eu>
2017-02-18 22:26:23 +0100

    Add Gunicorn as server dependency, model API routines.

--- api/main.py
+++ api/main.py
@@ -1,34 +1,24 @@
-import sys
 import zmq
-import random
-import time
+from flask import Flask
 
+# flask setup
+api = Flask(__name__)
+
+# zmq setup
 context = zmq.Context()
 workers = context.socket(zmq.PUSH)
 workers.bind("tcp://*:5557")
-
-for task_nbr in range(100):
-    workload = random.randint(1, 100)
-    workers.send_string(u'%i' % task_nbr)
-    print("Sending workload %d" % workload)
-
-
-# Socket to receive messages on
 receiver = context.socket(zmq.PULL)
-receiver.bind("tcp://*:5558")
+receiver.bind("tcp://*:5568")
 
-# Start our clock now
-tstart = time.time()
 
-# Process 100 confirmations
-for task_nbr in range(100):
-    s = receiver.recv()
-    if task_nbr % 10 == 0:
-        sys.stdout.write(':')
-    else:
-        sys.stdout.write('.')
-    sys.stdout.flush()
+@api.route("/", methods=['POST'])
+def rpn_solve():
+    workers.send_string("100")
+    return "sent"
 
-# Calculate and report duration of batch
-tend = time.time()
-print("Total elapsed time: %d msec" % ((tend-tstart)*1000))
+
+@api.route("/", methods=['GET'])
+def rpn_get_result():
+    result = receiver.recv()
+    return result
