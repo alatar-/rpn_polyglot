d5bbc35ed8c116c168771fd41b674d2616b2f3c5
alatar- <contact@alatar.eu>
2017-02-18 15:21:23 +0100

    Test flask with zeroMQ.

--- api/main.py
+++ api/main.py
@@ -1,17 +1,32 @@
 import zmq
+from flask import Flask
 
 context = zmq.Context()
+api = Flask(__name__)
 
-#  Socket to talk to server
-print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
 
-    #  Get the reply.
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+@api.route('/')
+def hello():
+    socket.send(b"Hello", 0)
+
+    i = 0
+    err = 0
+    while True:
+        try:
+            message = socket.recv(0)
+            i += 1
+            break
+            if str(message) == 'World':
+                break
+            print(message)
+        except:
+            err += 1
+            print("err++")
+    return str(message) + " " + str(err)
+
+
+if __name__ == "__main__":
+    api.run(host='0.0.0.0', port=8005, debug=True)
--- worker/src/github.com/alatar-/rpn/worker/worker.go
+++ worker/src/github.com/alatar-/rpn/worker/worker.go
@@ -3,6 +3,7 @@ package main
 import (
     zmq "github.com/pebbe/zmq4"
     "time"
+    "fmt"
 )
 
 func main() {
@@ -12,13 +13,26 @@ func main() {
     defer socket.Close()
     socket.Bind("tcp://*:5555")
 
+    fmt.Println("Server listening...")
     // Wait for messages
     for {
         msg, _ := socket.Recv(0)
         println("Received ", string(msg))
 
+        // foo := "Fooo"
+        // socket.Send(foo, 1)
+        // socket.Send(foo, 1)
+        // socket.Send(foo, 1)
+        // socket.Send(foo, 1)
+        // for i := 0; i < 2; i++ {
+            
+        //     // subscribers don't get all messages if publisher is too fast
+        //     // a one microsecond pause may still be too short
+        //     time.Sleep(time.Second)
+        // }
+
         // do some fake "work"
-        time.Sleep(time.Second)
+        time.Sleep(time.Microsecond)
 
         // send reply back to client
         reply := "World"
