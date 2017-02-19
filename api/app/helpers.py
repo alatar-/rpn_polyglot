3ad59c985ad94c672e6cb5a19206ba05e0d19e6a
alatar- <contact@alatar.eu>
2017-02-19 13:50:01 +0100

    Add logging.

---
+++ api/app/helpers.py
@@ -0,0 +1,23 @@
+import logging
+
+
+def configure_loggers():
+    default_format = '%(asctime)s %(levelname)s\t%(name)s:\t%(message)s'
+    default_date_format = '%H:%M:%S'
+    default_formatter = logging.Formatter(default_format, default_date_format )
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=default_format,
+        datefmt=default_date_format
+    )
+
+    requestsFileHandler = logging.FileHandler("./logs/requests.log")
+    requestsFileHandler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+    requestsFileHandler.setLevel(logging.DEBUG)
+    requestsLogger = logging.getLogger("requestsLogger")
+    requestsLogger.setLevel(logging.DEBUG)
+    requestsLogger.addHandler(requestsFileHandler)
+
+    consoleLogger = logging.getLogger("consoleLogger")
+    consoleLogger.setLevel(logging.INFO)
--- api/main.py
+++ api/main.py
@@ -1,54 +1,64 @@
+import logging
 import threading
 
 import zmq
 from flask import Flask
 
+from app import helpers
+
+
 # flask setup
 api = Flask(__name__)
+
+# get loggers
+requestsLogger = logging.getLogger("requestsLogger")
+consoleLogger = logging.getLogger("consoleLogger")
 
 # init local thread data
 # both zmq context and sockets must be local
 data = threading.local()
 
-# threads-shared results
+# threads-shared data
 results = []
 
 
 def sink_thread():
     global results
 
-    # Init sink socket
+    # Init sink socket for getting results from workers
     data.context = zmq.Context()  # new thread – new context
     data.receiver = data.context.socket(zmq.PULL)
     data.receiver.bind("tcp://*:5558")
     
-    print("Sink process running!")
+    consoleLogger.info("Sink process running!")
     while True:
         result = data.receiver.recv()
         results.append(str(result))
 
 
 def api_thread():
+    # Init socket for pushing input to workers
     data.context = zmq.Context()
     data.workers = data.context.socket(zmq.PUSH)
     data.workers.bind("tcp://*:5557")
 
-    print("API process running!")
+    consoleLogger.info("API process running!")
     api.run()
 
 
-@api.route("/solve_rpn", methods=['GET'])
+@api.route("/rpn/solve", methods=['GET'])
 def rpn_solve():
     data.workers.send_string("100")
     return "sent"
 
 
-@api.route("/get_rpn_result", methods=['GET'])
+@api.route("/rpn/collect", methods=['GET'])
 def rpn_get_result():
     return "".join(results)
 
 
 if __name__ == '__main__':
+    helpers.configure_loggers()
     thread = threading.Thread(target=sink_thread)
     thread.start()
     api_thread()
