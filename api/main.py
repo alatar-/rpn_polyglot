70c518d238cbf889483ea6a4f92631ae5f1aa8ca
alatar- <contact@alatar.eu>
2017-02-19 11:13:50 +0100

    Attempt to create extra sink worker with zeroMQ – not really roboust solution, falling back to two-workers approach.

--- api/main.py
+++ api/main.py
@@ -1,15 +1,46 @@
+import threading
+
 import zmq
 from flask import Flask
 
 # flask setup
 api = Flask(__name__)
 
-# zmq setup
-context = zmq.Context()
-workers = context.socket(zmq.PUSH)
-workers.bind("tcp://*:5557")
-receiver = context.socket(zmq.PULL)
-receiver.bind("tcp://*:5568")
+# init threading
+data = threading.local()
+lock = threading.Lock()
+
+
+def sink_thread():
+    print("Sink accessed!")
+    data.context = zmq.Context()  # new thread – new context
+    data.receiver = data.context.socket(zmq.PULL)
+    data.receiver.bind("tcp://*:5568")
+    while True:
+        result = data.receiver.recv()
+        print(result)
+
+
+with lock:
+    print("Inside lock")
+    if globals().get('sink_thread_running', True):
+        global sink_thread_running
+        sink_thread_running = False
+        print("Inside block scope")
+
+        data.context = zmq.Context()  # both zmq context and sockets must be local
+        data.workers = data.context.socket(zmq.PUSH)
+        data.workers.bind("tcp://*:5557")
+        
+        thread = threading.Thread(target=sink_thread)
+        thread.start()
+    else:
+        print("Inside second scope")
+        data.context = zmq.Context()  # both zmq context and sockets must be local
+        data.workers = data.context.socket(zmq.PUSH)
+        data.workers.connect("tcp://localhost:5557")
+
+print("Outside lock")
 
 
 @api.route("/", methods=['POST'])
@@ -20,5 +51,4 @@ def rpn_solve():
 
 @api.route("/", methods=['GET'])
 def rpn_get_result():
-    result = receiver.recv()
-    return result
+    return "null"
