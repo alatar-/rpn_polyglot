a11416d06f669ecd9a97187e77e419a4c14a3318
alatar- <contact@alatar.eu>
2017-02-20 15:44:36 +0100

    Refactor ruby, add logging.

--- client/README.md
+++ client/README.md
@@ -1,4 +1,16 @@
 ## Pre
 
-- ruby 2.4.0
-- `bundle install`
+- Ruby 2.4.0 
+- _Gemfile_ dependencies (`gem install bundler`, `bundle install`)
+
+## Run
+
+    ruby rpn_solver.rb < test.in
+
+#### test.in
+
+    4
+    1 2 -
+    1 2 / 4 * 5 + 3 -
+    3 3 / 1 -
+    3 2 -
--- client/rpn_solver.rb
+++ client/rpn_solver.rb
@@ -1,28 +1,54 @@
+require 'logger'
 require 'json'
+
 require 'rest-client'
 
-input = $stdin.readlines
+Host = "localhost:5000"
 
-response = RestClient.post "localhost:5000/rpn/solve", {'rpn' => input.join("")}.to_json , {content_type: :json, accept: :json}
+# Init logger
+$logger = Logger.new(STDOUT)
+$logger.level = Logger::DEBUG
 
-if response.code != 200
-    puts "Unexpected error"
-else
-    jobId = JSON.parse(response.body)['job_id']
 
-    while true do
-        response = RestClient.get "localhost:5000/rpn/collect/#{jobId}"
+def collectResponse(jobId)
+  while true do
+    response = RestClient.get("#{Host}/rpn/collect/#{jobId}")
+    $logger.debug("Collecting response... Return code: #{response.code}")
 
-        if response.code == 202
-            puts "Response not ready. Retrying..."
-            sleep(0.1)
-            next
-        elsif response.code == 200
-            puts JSON.parse(response.body)['result']['time']
-            puts JSON.parse(response.body)['result']['output']
-            break
-        else
-            puts "Unexpected error."
-        end
+    case response.code
+    when 202
+      $logger.debug("Response not ready. Retrying...")
+      sleep(0.1)
+    when 200
+      $logger.debug("Response collected.")
+      puts JSON.parse(response.body)['result']['time']
+      puts JSON.parse(response.body)['result']['output']
+      break
+    else
+      $logger.error("Undexpected API return code: #{response.code}")
+      break
     end
+  end
 end
+
+def main
+  $logger.debug("Reading input for RPN data...")
+  input = $stdin.readlines
+
+  $logger.debug("Input ready. Sending RPN job...")
+  response = RestClient.post("#{Host}/rpn/solve", {'rpn' => input.join()}.to_json , {content_type: :json, accept: :json})
+
+  case response.code
+  when 200
+    jobId = JSON.parse(response.body)['job_id']
+    $logger.info("Job scheduled. Assigned job id: #{jobId}")
+    collectResponse(jobId)
+  when 400
+    errMessage = response.body
+    $logger.error("Bad request: #{errMessage}")
+  else
+    $logger.error("Undexpected API return code #{response.code}")
+  end
+end
+
+main()
